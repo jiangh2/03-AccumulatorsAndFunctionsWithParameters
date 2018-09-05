76e08ac4487db3083e71b2f4889a1c802a0c915e
jiangh2 <Ydong6688@>
2018-09-05 18:39:50 -0400

    thursday

--- src/m7_summing.py
+++ src/m7_summing.py
@@ -4,9 +4,9 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
+         their colleagues and Hao Jiang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+import math
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -29,6 +29,20 @@ def run_test_sum_cosines():
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
 
+    expected = 0.13416
+    answer = sum_cosines(3)
+    print('test 1 expected', expected)
+    print('actual',answer)
+
+    expected = 1.124155469
+    answer = sum_cosines(2)
+    print('test 2 expected', expected)
+    print('actual',answer)
+
+    expected = 1.540302306
+    answer = sum_cosines(1)
+    print('test 3 expected', expected)
+    print('actual', answer)
 
 def sum_cosines(n):
     """
@@ -40,6 +54,11 @@ def sum_cosines(n):
       If n is 3, this function returns
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
+    total = 0
+    for k in range(n+1):
+        total = total + math.cos(k)
+    return total
+
     # ------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
